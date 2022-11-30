a90fb7671320a2757991f15e3739d02364ac5953
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-11-30 23:22:41 +0530

    lifi widget

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import { LiFiWidget } from '@lifi/widget';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <LiFiWidget
+      config={{
+        containerStyle: {
+          border: `1px solid rgb(234, 234, 234)`,
+          borderRadius: '16px',
+        },
+      }}
+    />
   );
 }
 
